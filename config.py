9a26aa44bdb7510595b68bd23e01884e181fdb92
sanogo souleymane <sanogotech@yahoo.fr>
2020-05-17 14:52:41 +0000

    Update

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 """Construct Redis connection URI."""
 from os import environ
 
+"""
 redis_host = environ.get('REDIS_HOST')
 redis_password = environ.get('REDIS_PASSWORD')
 redis_port = environ.get('REDIS_PORT')
 redis_db = environ.get('REDIS_DATABASE')
+"""
+
+redis_host = 'localhost'
+redis_password =''
+redis_port = 6379
+redis_db = 0
+
 redis_uri = f'redis://:{redis_host}@{redis_password}:{redis_port}/{redis_db}'
 
 redis_expiration = 604800
